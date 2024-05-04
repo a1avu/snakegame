514390c30345893194ec5e1415a56d8278df8174
a1avu <kenny042846@gmail.com>
2024-05-04 16:00:19 +0900

    v1

---
+++ snake_game.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>  //rand()
+#include<time.h>    // ??
+#include<windows.h> // gotoxy
+#include<conio.h> // 콘솔 입출력 getch()
+
+int main(void){
+
+
+    return 0;
+}
