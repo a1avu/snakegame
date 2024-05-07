c8cc975ecf4081ce9a465dac5b18ce43754db889
a1avu <kenny042846@gmail.com>
2024-05-07 13:52:50 +0900

    interface_nickname

--- snake_game.c
+++ snake_game.c
@@ -11,12 +11,12 @@
 
 
 //gotoxy함수 만들고 포인터 이용해서 원하는 텍스트 찍을 수 있게 했음
-void gotoxy(int x, int y, char* box){       
+void gotoxy(int x, int y, char* s){       
     COORD Pos;
     Pos.X = 2*x;
     Pos.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-    printf("%s", box);
+    printf("%s", s);
 }
 
 void inter_face(void);
@@ -30,7 +30,7 @@ int main(void){
 // interface 일단 만들기만 했습니당
 void inter_face(void){
     int i, j;
-
+    char nickname[3][10];
     for(i = MAP_X; i<=MAP_WIDTH; i++){
         gotoxy(i, MAP_Y, "■");
     }
@@ -41,8 +41,11 @@ void inter_face(void){
     for(i = MAP_X; i<=MAP_WIDTH; i++){
         gotoxy(i, MAP_Y + MAP_HEIGHT - 1, "■");
     }
-    gotoxy(14, 9, "★ 게임 시작 ★");
+    gotoxy(13, 9, "★ 게임 시작 ★");
     gotoxy(12, 11, "방향키로 조작 합니다.");
-    gotoxy(8, 13, "아무키나 입력하면 게임이 시작됩니다.");
+    gotoxy(8, 12, "아무키나 입력하면 게임이 시작됩니다.");
+    gotoxy(12, 15,"nickname : ");
+    scanf("%s",nickname);
+    gotoxy(15, 16, nickname);
     gotoxy(MAP_WIDTH, MAP_WIDTH, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
