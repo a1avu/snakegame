ebebf8aabe6ddb7ce6909de449f70e844c4a85d7
kimminki20 <mhn06159@naver.com>
2024-05-10 13:22:20 +0900

    충돌검사를 일단 지워두고 인터페이스에 마지막에 cls를 넣어 수정

--- snakemove2.c
+++ snakemove2.c
@@ -3,12 +3,13 @@
 #include<time.h>    //srand()
 #include<windows.h> // gotoxy
 #include<conio.h> // 콘솔 입출력 getch()
+#include <unistd.h>
 
 #define MAP_WIDTH 30
 #define MAP_HEIGHT 20
 #define MAP_X 3
 #define MAP_Y 2
-
+#define Initial_Length 5
 
 //지렁이의 위치를 나타내는 구조체
 typedef struct{
@@ -35,14 +36,20 @@ void gotoxy(int x, int y, char* s){
 
 void inter_face(char *nickname);
 void map(void);
-void moveSnake(Snake *snake, Direction direction);
+void moveSnake(Snake *snake, int length, Direction direction);
 
 int main(void){
     char nickname[20];
     inter_face(nickname);
     map();
 
-    Snake snake = {MAP_WIDTH/2, MAP_HEIGHT/2}; // 시작위치를 설정, 맵의 가로 중앙과 세로 중앙에서 시작
+    //지렁이 초기 위치 및 몸 길이
+    Snake snake[Initial_Length];
+    for (int i = 0; i < Initial_Length; i++) {
+        snake[i].x = MAP_WIDTH/2 - i; //초기에 머리가 가장 오른쪽에 위치 몸통이 왼쪽으로 이어지도록 하기 위함. i가 증가함에 따라 몸통의 좌표가 왼쪽으로 하나씩 이동
+        snake[i].y = MAP_HEIGHT/2;
+    }
+    int snakeLength = Initial_Length; //나중에 먹이 먹어서 몸 길어질때 사용할 변수.
     Direction direction = RIGHT; //오른쪽으로 이동하게 함.
 
     while(1){
@@ -64,15 +71,16 @@ int main(void){
                 default:
                     break;
             }
+        }
+        moveSnake(snake, snakeLength, direction);
     }
-    moveSnake(&snake, direction);
-    map();
     return 0;
 }
-}
+
 
 // interface 일단 만들기만 했습니당
 void inter_face(char *nickname){
+    system("cls");
     int i, j;
     for(i = MAP_X; i<=MAP_WIDTH; i++){
         gotoxy(i, MAP_Y, "■");
@@ -87,16 +95,13 @@ void inter_face(char *nickname){
     gotoxy(13, 9, "★ 게임 시작 ★");
     gotoxy(12, 11, "방향키로 조작 합니다.");
     gotoxy(8, 12, "아무키나 입력하면 게임이 시작됩니다.");
-    gotoxy(12, 15,"nickname : ");
-    scanf("%s",nickname);
-    gotoxy(15, 16, nickname);
-    gotoxy(MAP_WIDTH, MAP_WIDTH, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    gotoxy(12, 15, "nickname : ");
+    scanf("%s", nickname);
+    gotoxy(12 +10, 15 , nickname); // 입력받은 닉네임 출력
     system("cls");
 }
 void map(void){
-    system("cls");
     int i, j;
-    char nickname[3][10];
     for(i = MAP_X; i<=MAP_WIDTH; i++){
         gotoxy(i, MAP_Y, "■");
     }
@@ -107,29 +112,54 @@ void map(void){
     for(i = MAP_X; i<=MAP_WIDTH; i++){
         gotoxy(i, MAP_Y + MAP_HEIGHT - 1, "■");
     }
-    gotoxy(40, 15, "nickname: ");
-    printf("%s", nickname);
-    gotoxy(MAP_WIDTH, MAP_WIDTH, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
-void moveSnake(Snake *snake, Direction direction){
-     int preX = snake -> x;
-     int preY = snake -> y;
-     switch(direction) {
+void moveSnake(Snake *snake, int length, Direction direction){
+    // 현재 지렁이의 머리 위치
+    int preX = snake[0].x;
+    int preY = snake[0].y;
+    //방향키에 따라서 머리 위치 업데이트
+    switch(direction) {
         case UP:
-            snake->y--;
+            snake[0].y--;
             break;
         case DOWN:
-            snake->y++;
+            snake[0].y++;
             break;
         case LEFT:
-            snake->x--;
+            snake[0].x--;
             break;
         case RIGHT:
-            snake->x++;
+            snake[0].x++;
             break;
         default:
             break;
-}
-gotoxy(preX, preY, " "); //이전에 있던 지렁이의 위치를 지움
-gotoxy(snake ->x, snake  -> y,"■"); //새로운 위치의 지렁이를 그림.
+    }
+    /*충돌 검사
+    for (int i = 1; i < length; i++) {
+        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+            // 충돌 발생 시 게임 종료 등의 처리를 해주어야 합니다.
+            // 여기서는 간단하게 프로그램 종료로 처리합니다.
+            exit(0);
+        }
+    }*/
+    // 이전 꼬리의 위치
+    int preTailX = snake[length - 1].x;
+    int preTailY = snake[length - 1].y;
+
+    gotoxy(preTailX, preTailY, " "); // 이전 꼬리 지우기
+
+    // 몸통 이동
+    for (int i = length - 1; i > 0; i--) {
+        snake[i].x = snake[i - 1].x;
+        snake[i].y = snake[i - 1].y;
+    }
+
+    // 새로운 머리 그리기
+    gotoxy(snake[0].x, snake[0].y ,"■");
+
+    // 몸통 그리기
+    for (int i = 1; i <= length - 1; i++) {
+        gotoxy(snake[i].x, snake[i].y, "■");
+    }
+    Sleep(1000);
 }
