e838f571bbd830ef0b7b226e6e64b637523b9fa3
a1avu <kenny042846@gmail.com>
2024-05-10 14:07:12 +0900

    nicknamecomplete

--- snake_game.c
+++ snake_game.c
@@ -8,7 +8,20 @@
 #define MAP_HEIGHT 20
 #define MAP_X 3
 #define MAP_Y 2
+#define Initial_Length 5
 
+//지렁이의 위치를 나타내는 구조체
+typedef struct{
+    int x;
+    int y;
+    }Snake;
+// 지렁이의 이동방향을 나타내는 열거형
+typedef enum{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direction;
 
 //gotoxy함수 만들고 포인터 이용해서 원하는 텍스트 찍을 수 있게 했음
 void gotoxy(int x, int y, char* s){       
@@ -26,11 +39,45 @@ int main(void){
     inter_face();
     map();
 
+    
+    //지렁이 초기 위치 및 몸 길이
+    Snake snake[Initial_Length];
+    for (int i = 0; i < Initial_Length; i++) {
+        snake[i].x = MAP_WIDTH/2 - i; //초기에 머리가 가장 오른쪽에 위치 몸통이 왼쪽으로 이어지도록 하기 위함. i가 증가함에 따라 몸통의 좌표가 왼쪽으로 하나씩 이동
+        snake[i].y = MAP_HEIGHT/2;
+    }
+    int snakeLength = Initial_Length; //나중에 먹이 먹어서 몸 길어질때 사용할 변수.
+    Direction direction = RIGHT; //오른쪽으로 이동하게 함.
+
+    while(1){
+        if (_kbhit()) { // 사용자 입력 감지
+            char ch = _getch();
+            switch(ch) {
+                case 72: // 방향키 위쪽
+                    direction = UP;
+                    break;
+                case 80: // 방향키 아래쪽
+                    direction = DOWN;
+                    break;
+                case 75: // 방향키 왼쪽
+                    direction = LEFT;
+                    break;
+                case 77: // 방향키 오른쪽
+                    direction = RIGHT;
+                    break;
+                default:
+                    break;
+            }
+        }
+        moveSnake(snake, snakeLength, direction);
+    }
+
     return 0;
 }
 
 // interface 일단 만들기만 했습니당
 void inter_face(void){
+    system("cls");
     int i, j;
     char nickname[3][10];
     for(i = MAP_X; i<=MAP_WIDTH; i++){
@@ -51,6 +98,7 @@ void inter_face(void){
     gotoxy(15, 16, nickname);
     gotoxy(MAP_WIDTH, MAP_WIDTH, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
+
 void map(void){
     system("cls");
     int i, j;
@@ -68,4 +116,55 @@ void map(void){
     gotoxy(40, 15, "nickname: ");
     printf("%s", nickname);
     gotoxy(MAP_WIDTH, MAP_WIDTH, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+}
+
+void moveSnake(Snake *snake, int length, Direction direction){
+    // 현재 지렁이의 머리 위치
+    int preX = snake[0].x;
+    int preY = snake[0].y;
+    //방향키에 따라서 머리 위치 업데이트
+    switch(direction) {
+        case UP:
+            snake[0].y--;
+            break;
+        case DOWN:
+            snake[0].y++;
+            break;
+        case LEFT:
+            snake[0].x--;
+            break;
+        case RIGHT:
+            snake[0].x++;
+            break;
+        default:
+            break;
+    }
+      // 충돌 검사
+    for (int i = 1; i < length; i++) {
+        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+            // 충돌 발생 시 게임 종료 등의 처리를 해주어야 합니다.
+            // 여기서는 간단하게 프로그램 종료로 처리합니다.
+            exit(0);
+        }
+    }
+    // 이전 꼬리의 위치
+    int preTailX = snake[length - 1].x;
+    int preTailY = snake[length - 1].y;
+
+    gotoxy(preTailX, preTailY, " "); // 이전 꼬리 지우기
+
+    // 몸통 이동
+    for (int i = length - 1; i > 0; i--) {
+        snake[i].x = snake[i - 1].x;
+        snake[i].y = snake[i - 1].y;
+    }
+
+    // 새로운 머리 그리기
+    gotoxy(snake[0].x, snake[0].y ,"■");
+
+    // 몸통 그리기
+    for (int i = 1; i <= length - 1; i++) {
+        gotoxy(snake[i].x, snake[i].y, "■");
+    }
+    Sleep(1000);
 }
